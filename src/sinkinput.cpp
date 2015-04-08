686d250eee2da72d189a93c22f52231f556b538b
Harald Sitter <sitter@kde.org>
2015-04-08 14:58:07 +0200

    fix base bug, forgot to call base in override

--- src/sinkinput.cpp
+++ src/sinkinput.cpp
@@ -2,5 +2,6 @@
 
 void SinkInput::setInfo(const pa_sink_input_info *info)
 {
+    StreamBase<pa_sink_input_info>::setInfo(info);
     m_sinkIndex = info->sink;
 }
--- src/sourceoutput.cpp
+++ src/sourceoutput.cpp
@@ -2,5 +2,6 @@
 
 void SourceOutput::setInfo(const pa_source_output_info *info)
 {
+    StreamBase<pa_source_output_info>::setInfo(info);
     m_sourceIndex = info->source;
 }
