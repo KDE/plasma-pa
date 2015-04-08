2450c5959d90518e8a52f641298d971416a4bd5d
Harald Sitter <sitter@kde.org>
2015-04-08 14:02:43 +0200

    move sinkport to portbase

--- src/sink.cpp
+++ src/sink.cpp
@@ -2,22 +2,6 @@
 
 #include <QDebug>
 
-SinkPort::SinkPort()
-    : m_name()
-    , m_description()
-    , m_priority(0)
-    , m_isAvailable(false)
-{
-}
-
-void SinkPort::setInfo(const pa_sink_port_info *port)
-{
-    m_name = port->name;
-    m_description = port->description;
-    m_priority = port->priority;
-    m_isAvailable = port->available;
-}
-
 Sink::Sink()
     : m_name()
     , m_description()
@@ -39,7 +23,7 @@ void Sink::setInfo(const pa_sink_info *info)
 
     m_ports.clear();
     for (auto **ports = info->ports; *ports != nullptr; ++ports) {
-        SinkPort port;
+        Port port;
         port.setInfo(*ports);
         m_ports.append(port);
         if (info->active_port == *ports) {
--- src/sink.h
+++ src/sink.h
@@ -3,31 +3,15 @@
 
 #include <pulse/volume.h>
 
+#include "portbase.h"
 #include "pulseobject.h"
-
-class Q_DECL_EXPORT SinkPort
-{
-public:
-    SinkPort();
-
-    void setInfo(const pa_sink_port_info *port);
-
-    QString name() const { return m_name; }
-    QString description() const { return m_description; }
-    quint32 priority() const { return m_priority; }
-    bool isAvailable() const { return m_isAvailable; }
-
-private:
-    QString m_name;
-    QString m_description;
-    quint32 m_priority;
-    bool m_isAvailable;
-};
 
 class Q_DECL_EXPORT Sink : public PulseObject
 {
     Q_OBJECT
 public:
+    typedef PortBase<pa_sink_port_info> Port;
+
     Sink();
 
     void setInfo(const pa_sink_info *info);
@@ -36,15 +20,15 @@ public:
     QString description() const { return m_description; }
     pa_cvolume volume() const { return m_volume; }
     bool isMuted() const { return m_isMuted; }
-    QList<SinkPort> ports() const { return m_ports; }
-    bool activePortIndex() const { return m_activePortIndex; }
+    QList<Port> ports() const { return m_ports; }
+    int activePortIndex() const { return m_activePortIndex; }
 
 private:
     QString m_name;
     QString m_description;
     pa_cvolume m_volume;
     bool m_isMuted;
-    QList<SinkPort> m_ports;
+    QList<Port> m_ports;
     int m_activePortIndex;
 };
 
