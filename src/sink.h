4175d3e300bb788176e1f4ffc8808e37af25afc5
Harald Sitter <sitter@kde.org>
2015-04-15 12:15:54 +0200

    expose volume properties on devices

--- src/sink.h
+++ src/sink.h
@@ -11,7 +11,7 @@ class Q_DECL_EXPORT Sink : public DeviceBase<pa_sink_info, pa_sink_port_info>
     Q_PROPERTY(QList<Port> ports READ ports)
     Q_PROPERTY(int activePortIndex READ activePortIndex)
 
-//    Q_PROPERTY(qint64 volume READ volume WRITE setVolume)
+    Q_PROPERTY(qint64 volume READ volume WRITE setVolume)
     Q_PROPERTY(bool muted READ isMuted WRITE setMuted)
 public:
     void setVolume(qint64 volume) Q_DECL_OVERRIDE;
--- src/source.h
+++ src/source.h
@@ -11,7 +11,7 @@ class Q_DECL_EXPORT Source : public DeviceBase<pa_source_info, pa_source_port_in
     Q_PROPERTY(QList<Port> ports READ ports)
     Q_PROPERTY(int activePortIndex READ activePortIndex)
 
-//    Q_PROPERTY(qint64 volume READ volume WRITE setVolume)
+    Q_PROPERTY(qint64 volume READ volume WRITE setVolume)
     Q_PROPERTY(bool muted READ isMuted WRITE setMuted)
 public:
     void setVolume(qint64 volume) Q_DECL_OVERRIDE;
