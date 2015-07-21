454a57b0dc93363ee5c1dfdf2626461ff0b6e01e
Harald Sitter <sitter@kde.org>
2015-07-21 10:07:27 +0200

    get cardindex of devices
    this allows closer icon_name mapping by translating device->card->port and
    then reading properties of the port. device port infromation is limited to
    name which is meh...

--- src/device.h
+++ src/device.h
@@ -14,6 +14,7 @@ class Q_DECL_EXPORT Device : public VolumeObject
     Q_OBJECT
     Q_PROPERTY(QString name READ name NOTIFY nameChanged)
     Q_PROPERTY(QString description READ description NOTIFY descriptionChanged)
+    Q_PROPERTY(quint32 cardIndex READ cardIndex NOTIFY cardIndexChanged)
     Q_PROPERTY(QList<QObject *> ports READ ports NOTIFY portsChanged)
     Q_PROPERTY(quint32 activePortIndex READ activePortIndex WRITE setActivePortIndex NOTIFY activePortIndexChanged)
 public:
@@ -32,6 +33,9 @@ public:
             m_description = info->description;
             emit descriptionChanged();
         }
+
+        m_cardIndex = info->card;
+        emit cardIndexChanged();
 
         // TODO: this rebuilds the entire port list on every update. would be
         // nicer if it actually removed what needs removing updates what needs
@@ -53,6 +57,7 @@ public:
 
     QString name() const { return m_name; }
     QString description() const { return m_description; }
+    quint32 cardIndex() const { return m_cardIndex; }
     QList<QObject *> ports() const { return m_ports; }
     quint32 activePortIndex() const { return m_activePortIndex; }
     virtual void setActivePortIndex(quint32 port_index) = 0;
@@ -60,6 +65,7 @@ public:
 signals:
     void nameChanged();
     void descriptionChanged();
+    void cardIndexChanged();
     void portsChanged();
     void activePortIndexChanged();
 
@@ -69,6 +75,7 @@ protected:
 private:
     QString m_name;
     QString m_description;
+    quint32 m_cardIndex = -1;
     QList<QObject *> m_ports;
     quint32 m_activePortIndex = -1;
 };
