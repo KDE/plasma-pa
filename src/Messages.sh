821836dd3c576e502879743e9a39b06f76954b16
David Edmundson <kde@davidedmundson.co.uk>
2015-08-01 17:05:25 +0200

    Use same .pot name as AboutData

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT `find . -name \*.qml -name \*.cpp` -o $podir/kcm_plasma_volume.pot
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/kcm_pulseaudio.pot
 rm -f rc.cpp
