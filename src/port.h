KDE/plasma-pa

commit 206a00f9585811f2851b9ce62f6d634cd3c7e822

Move availability property from Port to Profile
Differential Revision: https://phabricator.kde.org/D9671

src/port.h
@@ -31,14 +31,8 @@ namespace QPulseAudio
 class Port : public Profile
 {
     Q_OBJECT
-    Q_PROPERTY(Availability availability READ availability NOTIFY availabilityChanged)
+
 public:
-    enum Availability {
-        Unknown,
-        Available,
-        Unavailable
-    };
-    Q_ENUM(Availability)
 
     Port(QObject *parent);
     virtual ~Port();
@@ -47,31 +41,7 @@ public:
     void setInfo(const PAInfo *info)
     {
         Profile::setInfo(info);
-
-        Availability newAvailability;
-        switch (info->available) {
-        case PA_PORT_AVAILABLE_NO:
-            newAvailability = Unavailable;
-            break;
-        case PA_PORT_AVAILABLE_YES:
-            newAvailability = Available;
-            break;
-        default:
-            newAvailability = Unknown;
-        }
-        if (m_availability != newAvailability) {
-            m_availability = newAvailability;
-            emit availabilityChanged();
-        }
     }
-
-    Availability availability() const;
-
-signals:
-    void availabilityChanged();
-
-private:
-    Availability m_availability;
 };
 
 } // QPulseAudio

src/profile.h
@@ -33,7 +33,16 @@ class Profile : public QObject
     Q_PROPERTY(QString name READ name NOTIFY nameChanged)
     Q_PROPERTY(QString description READ description NOTIFY descriptionChanged)
     Q_PROPERTY(quint32 priority READ priority NOTIFY priorityChanged)
+    Q_PROPERTY(Availability availability READ availability NOTIFY availabilityChanged)
 public:
+
+    enum Availability {
+        Unknown,
+        Available,
+        Unavailable
+    };
+    Q_ENUM(Availability)
+
     Profile(QObject *parent);
     virtual ~Profile();
 
@@ -58,21 +67,30 @@ public:
             m_priority = info->priority;
             emit priorityChanged();
         }
+
+        Availability newAvailability = info->available ? Available : Unavailable;
+        if (m_availability != newAvailability) {
+            m_availability = newAvailability;
+            emit availabilityChanged();
+        }
     }
 
     QString name() const;
     QString description() const;
     quint32 priority() const;
+    Availability availability() const;
 
 signals:
     void nameChanged();
     void descriptionChanged();
     void priorityChanged();
+    void availabilityChanged();
 
 private:
     QString m_name;
     QString m_description;
     quint32 m_priority;
+    Availability m_availability;
 };
 
 } // QPulseAudio

src/port.cpp
@@ -25,17 +25,11 @@ namespace QPulseAudio
 
 Port::Port(QObject *parent)
     : Profile(parent)
-    , m_availability(Unknown)
 {
 }
 
 Port::~Port()
 {
-}
-
-Port::Availability Port::availability() const
-{
-    return m_availability;
 }
 
 } // QPulseAudio

src/profile.cpp
@@ -28,6 +28,7 @@ Profile::Profile(QObject *parent)
     , m_name()
     , m_description()
     , m_priority(0)
+    , m_availability(Unknown)
 {
 }
 
@@ -48,6 +49,11 @@ QString Profile::description() const
 quint32 Profile::priority() const
 {
     return m_priority;
+}
+
+Profile::Availability Profile::availability() const
+{
+    return m_availability;
 }
 
 } // QPulseAudio

src/qml/plugin.cpp
@@ -28,7 +28,7 @@
 #include "source.h"
 #include "context.h"
 #include "modulemanager.h"
-#include "port.h"
+#include "profile.h"
 
 #include "globalactioncollection.h"
 #include "volumeosd.h"
@@ -55,7 +55,7 @@ void Plugin::registerTypes(const char* uri)
     qmlRegisterType<QPulseAudio::SourceOutputModel>(uri, 0, 1, "SourceOutputModel");
     qmlRegisterType<QPulseAudio::StreamRestoreModel>(uri, 0, 1, "StreamRestoreModel");
     qmlRegisterType<QPulseAudio::ModuleModel>(uri, 0, 1, "ModuleModel");
-    qmlRegisterUncreatableType<QPulseAudio::Port>(uri, 0, 1, "Port", QString());
+    qmlRegisterUncreatableType<QPulseAudio::Profile>(uri, 0, 1, "Profile", QString());
     qmlRegisterType<GlobalAction>(uri, 0, 1, "GlobalAction");
     qmlRegisterType<GlobalActionCollection>(uri, 0, 1, "GlobalActionCollection");
     qmlRegisterType<VolumeOSD>(uri, 0, 1, "VolumeOSD");

commit ba737f478208b37826f14c929ab8611a2cb20798

Fix wrong availability of profiles and ports.
Summary: Fixed bug where available ports are displayed as unavailable/unplugged and vice versa. It seems that this bug was introduced with D9671.

Reviewers: nicolasfella, broulik, drosca

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D13694

src/port.h
@@ -40,7 +40,18 @@ public:
     template<typename PAInfo>
     void setInfo(const PAInfo *info)
     {
-        Profile::setInfo(info);
+        Availability newAvailability;
+        switch (info->available) {
+        case PA_PORT_AVAILABLE_NO:
+            newAvailability = Unavailable;
+            break;
+        case PA_PORT_AVAILABLE_YES:
+            newAvailability = Available;
+            break;
+        default:
+            newAvailability = Unknown;
+        }
+        setCommonInfo(info, newAvailability);
     }
 };
 

src/profile.h
@@ -49,6 +49,24 @@ public:
     template<typename PAInfo>
     void setInfo(const PAInfo *info)
     {
+        setCommonInfo(info, info->available ? Available : Unavailable);
+    }
+
+    QString name() const;
+    QString description() const;
+    quint32 priority() const;
+    Availability availability() const;
+
+signals:
+    void nameChanged();
+    void descriptionChanged();
+    void priorityChanged();
+    void availabilityChanged();
+
+protected:
+    template<typename PAInfo>
+    void setCommonInfo(const PAInfo *info, Availability newAvailability)
+    {
         // Description is optional. Name not so much as we need some ID.
         Q_ASSERT(info->name);
         QString infoName = QString::fromUtf8(info->name);
@@ -68,23 +86,11 @@ public:
             emit priorityChanged();
         }
 
-        Availability newAvailability = info->available ? Available : Unavailable;
         if (m_availability != newAvailability) {
             m_availability = newAvailability;
             emit availabilityChanged();
         }
     }
-
-    QString name() const;
-    QString description() const;
-    quint32 priority() const;
-    Availability availability() const;
-
-signals:
-    void nameChanged();
-    void descriptionChanged();
-    void priorityChanged();
-    void availabilityChanged();
 
 private:
     QString m_name;