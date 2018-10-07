604f35edd4a14d630d4583f61eb8109f21b6ed3a
Yuri Chornoivan <yurchor@ukr.net>
2018-10-07 09:05:28 +0300

    Make widget configuration translatable

--- applet/Messages.sh
+++ applet/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.js -o -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.volume.pot
-rm -f rc.cpp
+$XGETTEXT `find . -name \*.js -o -name \*.qml` `find ../src/kcm/package/contents/ui -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.volume.pot
