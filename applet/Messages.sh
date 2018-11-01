9d7629c150466c45ca53e7ceb2199992dacc42e0
Albert Astals Cid <aacid@kde.org>
2018-11-01 19:56:48 +0100

    Revert 604f35edd4a14d630d4583f61eb8109f21b6ed3a
    That's not the real fix
    
    BUGS: 399756

--- applet/Messages.sh
+++ applet/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.js -o -name \*.qml` `find ../src/kcm/package/contents/ui -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.volume.pot
+$XGETTEXT `find . -name \*.js -o -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.volume.pot
