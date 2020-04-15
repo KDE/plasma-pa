KDE/plasma-pa

commit fbdef07f554bbec59d6142837fdf44afd103f6dc

[StreamRestore] Don't mess with channels
Just set volume for all of them.
Otherwise when just calling setMuted, we will send the original volume struct, which might have multiple channels,
with a hardcoded channel map that only has one channel. PulseAudio then refuses to write the settings.

Patch ported from pulseaudio-qt

BUG: 398392
FIXED-IN: 5.15.5

Differential Revision: https://phabricator.kde.org/D20450

src/streamrestore.cpp
@@ -100,8 +100,9 @@ qint64 StreamRestore::volume() const
 void StreamRestore::setVolume(qint64 volume)
 {
     pa_cvolume vol = m_cache.valid ? m_cache.volume : m_volume;
-    vol.channels = 1;
-    vol.values[0] = volume;
+    for (int i = 0; i < vol.channels; ++i) {
+        vol.values[i] = volume;
+    }
 
     if (m_cache.valid) {
         writeChanges(vol, m_cache.muted, m_cache.device);
@@ -184,8 +185,7 @@ void StreamRestore::writeChanges(const pa_cvolume &volume, bool muted, const QSt
 
     pa_ext_stream_restore_info info;
     info.name = nameData.constData();
-    info.channel_map.channels = 1;
-    info.channel_map.map[0] = PA_CHANNEL_POSITION_MONO;
+    info.channel_map = m_channelMap;
     info.volume = volume;
     info.device = deviceData.isEmpty() ? nullptr : deviceData.constData();
     info.mute = muted;

commit 5bdd1368957955c900d5a17bf6c890bb0920d356

Create StreamRestore channel if none exists
Summary:
We need one channel to be able to control the volume. This restores D7485 which was broken by D20450 while fixing a different issue.
This patch tries to combine the two by only creating a new channel if none exists and not messing with existing ones.

BUG: 407397

Test Plan: Can control the notification volume in the KCM now. The slider position matches the one in pavucontrol

Reviewers: #plasma, drosca, broulik, ngraham

Reviewed By: ngraham

Subscribers: ngraham, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D28822

src/streamrestore.cpp
@@ -100,6 +100,13 @@ qint64 StreamRestore::volume() const
 void StreamRestore::setVolume(qint64 volume)
 {
     pa_cvolume vol = m_cache.valid ? m_cache.volume : m_volume;
+
+    // If no channel exists force one. We need one to be able to control the volume
+    // See https://bugs.kde.org/show_bug.cgi?id=407397
+    if (vol.channels == 0) {
+        vol.channels = 1;
+    }
+
     for (int i = 0; i < vol.channels; ++i) {
         vol.values[i] = volume;
     }
@@ -189,6 +196,13 @@ void StreamRestore::writeChanges(const pa_cvolume &volume, bool muted, const QSt
     info.volume = volume;
     info.device = deviceData.isEmpty() ? nullptr : deviceData.constData();
     info.mute = muted;
+
+    // If no channel exists force one. We need one to be able to control the volume
+    // See https://bugs.kde.org/show_bug.cgi?id=407397
+    if (info.channel_map.channels == 0) {
+        info.channel_map.channels = 1;
+        info.channel_map.map[0] = PA_CHANNEL_POSITION_MONO;
+    }
 
     m_cache.valid = true;
     m_cache.volume = volume;