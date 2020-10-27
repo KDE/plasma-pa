Sonic-DE/sonic-audio-applet-pulse

[kcm] Remove usage of tabs
The usage of tabs comes with quite some visual and technical problems. In general we tend to move away from tabs in KCMs for that reason.

The Device and Applications lists are combined into a single list that is shown as the initial view. The former Advanced tab is opened as a new page when clicking a Configure button. This is a pattern we use in other KCMs as well.

The content of the lists and the advanced tab are unchanged for now, but this opens the door for future further improvements

src/kcm/package/contents/ui/main.qml
@@ -1,6 +1,8 @@
 /*
     Copyright 2014-2015 Harald Sitter <sitter@kde.org>
+    Copyright 2016 David Rosca <nowrep@gmail.com>
     Copyright 2019 Sefa Eyeoglu <contact@scrumplex.net>
+    Copyright 2020 Nicolas Fella <nicolas.fella@gmx.de>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
@@ -23,68 +25,199 @@ import QtQuick 2.7
 import QtQuick.Layouts 1.3
 import QtQuick.Controls 2.0
 
-import org.kde.kcm 1.0
+import org.kde.kcm 1.3
 import org.kde.plasma.core 2.0 as PlasmaCore /* for units.gridUnit */
 import org.kde.kirigami 2.5 as Kirigami
 import org.kde.plasma.private.volume 0.1
 
-Kirigami.Page {
-    title: kcm.name
-    property QtObject paSinkModel: SinkModel { }
-    property QtObject paSourceModel: SourceModel { }
+ScrollViewKCM {
+
     property int maxVolumeValue: PulseAudio.NormalVolume // the applet supports changing this value. We will just assume 65536 (100%)
     ConfigModule.quickHelp: i18nd("kcm_pulseaudio", "This module allows configuring the Pulseaudio sound subsystem.")
+
     implicitHeight: Kirigami.Units.gridUnit * 28
     implicitWidth: Kirigami.Units.gridUnit * 28
 
-    // TODO: replace this TabBar-plus-Frame-in-a-ColumnLayout with whatever shakes
-    // out of https://bugs.kde.org/show_bug.cgi?id=394296
-    ColumnLayout {
-        anchors.fill: parent
-        spacing: 0
+    SinkModel {
+        id: paSinkModel
+    }
 
-        TabBar {
-            id: tabView
+    SourceModel {
+        id: paSourceModel
+    }
 
-            // Tab styles generally assume that they're touching the inner layout,
-            // not the frame, so we need to move the tab bar down a pixel and make
-            // sure it's drawn on top of the frame
-            Layout.bottomMargin: -1
-            z: 1
+    PulseObjectFilterModel {
+        id: paSinkFilterModel
 
-            TabButton {
-                text: i18ndc("kcm_pulseaudio", "@title:tab", "Devices")
+        sortRole: "SortByDefault"
+        sortOrder: Qt.DescendingOrder
+        filterOutInactiveDevices: true
+        sourceModel: paSinkModel
+    }
+
+    PulseObjectFilterModel {
+        id: paSourceFilterModel
+
+        sortRole: "SortByDefault"
+        sortOrder: Qt.DescendingOrder
+        filterOutInactiveDevices: true
+        sourceModel: paSourceModel
+    }
+
+    view: Flickable {
+        id: flickable
+
+        contentWidth: column.width
+        contentHeight: column.height
+
+        ColumnLayout {
+            id: column
+
+            width: flickable.width
+
+            Header {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                Layout.fillWidth: true
+                enabled: sinks.count > 0
+                text: i18nd("kcm_pulseaudio", "Playback Devices")
+                disabledText: i18ndc("kcm_pulseaudio", "@label", "No Playback Devices Available")
             }
-            TabButton {
-                text: i18ndc("kcm_pulseaudio", "@title:tab", "Applications")
+
+            ListView {
+                id: sinks
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                Layout.fillWidth: true
+                Layout.preferredHeight: contentHeight
+                interactive: false
+                spacing: Kirigami.Units.gridUnit
+                model: inactiveDevicesButton.checked || !inactiveDevicesButton.visible ? paSinkModel : paSinkFilterModel
+                delegate: DeviceListItem {
+                    isPlayback: true
+                }
             }
-            TabButton {
-                text: i18ndc("kcm_pulseaudio", "@title:tab", "Advanced")
+
+            Header {
+                Layout.fillWidth: true
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                enabled: sources.count > 0
+                text: i18nd("kcm_pulseaudio", "Recording Devices")
+                disabledText: i18ndc("kcm_pulseaudio", "@label", "No Recording Devices Available")
+            }
+
+            ListView {
+                id: sources
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                Layout.fillWidth: true
+                Layout.preferredHeight: contentHeight
+                interactive: false
+                spacing: Kirigami.Units.gridUnit
+                model: inactiveDevicesButton.checked || !inactiveDevicesButton.visible ? paSourceModel : paSourceFilterModel
+                delegate: DeviceListItem {
+                    isPlayback: false
+                }
+            }
+
+            Button {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                id: inactiveDevicesButton
+
+                Layout.alignment: Qt.AlignHCenter
+
+                checkable: true
+
+                text: i18nd("kcm_pulseaudio", "Show Inactive Devices")
+                icon.name: "view-visible"
+
+                // Only show if there actually are any inactive devices
+                visible: (paSourceModel.count != paSourceFilterModel.count) || (paSinkModel.count != paSinkFilterModel.count)
+            }
+
+            Header {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                Layout.fillWidth: true
+                enabled: eventStreamView.count || sinkInputView.count
+                text: i18nd("kcm_pulseaudio", "Playback Streams")
+                disabledText: i18ndc("kcm_pulseaudio", "@label", "No Applications Playing Audio")
+            }
+
+            ListView {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                id: eventStreamView
+                Layout.fillWidth: true
+                Layout.preferredHeight: contentHeight
+                interactive: false
+                spacing: units.largeSpacing
+                model: PulseObjectFilterModel {
+                    filters: [ { role: "Name", value: "sink-input-by-media-role:event" } ]
+                    sourceModel: StreamRestoreModel {}
+                }
+                delegate: StreamListItem {
+                    deviceModel: paSinkModel
+                    isPlayback: true
+                }
+            }
+
+            ListView {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                id: sinkInputView
+                Layout.fillWidth: true
+                Layout.preferredHeight: contentHeight
+                interactive: false
+                spacing: units.largeSpacing
+                model: PulseObjectFilterModel {
+                    filters: [ { role: "VirtualStream", value: false } ]
+                    sourceModel: SinkInputModel {}
+                }
+                delegate: StreamListItem {
+                    deviceModel: paSinkModel
+                    isPlayback: true
+                }
+            }
+
+            Header {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                Layout.fillWidth: true
+                enabled: sourceOutputView.count > 0
+                text: i18nd("kcm_pulseaudio", "Recording Streams")
+                disabledText: i18ndc("kcm_pulseaudio", "@label", "No Applications Recording Audio")
+            }
+
+            ListView {
+                Layout.leftMargin: Kirigami.Units.largeSpacing
+                Layout.rightMargin: Kirigami.Units.largeSpacing
+                id: sourceOutputView
+                Layout.fillWidth: true
+                Layout.preferredHeight: contentHeight
+                interactive: false
+                spacing: units.largeSpacing
+                model: PulseObjectFilterModel {
+                    filters: [ { role: "VirtualStream", value: false } ]
+                    sourceModel: SourceOutputModel {}
+                }
+
+                delegate: StreamListItem {
+                    deviceModel: sourceModel
+                    isPlayback: false
+                }
             }
         }
-        Frame {
-            Layout.fillWidth: true
-            Layout.fillHeight: true
+    }
 
-            StackLayout {
-                anchors.fill: parent
-                anchors.margins: -Kirigami.Units.smallSpacing
-
-                currentIndex: tabView.currentIndex
-
-                Devices {
-                    Layout.fillWidth: true
-                    Layout.fillHeight: true
-                }
-                Applications {
-                    Layout.fillWidth: true
-                    Layout.fillHeight: true
-                }
-                Advanced {
-                    Layout.fillWidth: true
-                    Layout.fillHeight: true
-                }
-            }
+    footer: RowLayout {
+        Button {
+            text: i18n("Configure...")
+            icon.name: "configure"
+            Layout.alignment: Qt.AlignRight
+            onClicked: kcm.push("Advanced.qml")
         }
     }
 }

KDE/plasma-pa

src/kcm/package/contents/ui/main.qml
@@ -1,6 +1,8 @@
 /*
     Copyright 2014-2015 Harald Sitter <sitter@kde.org>
+    Copyright 2016 David Rosca <nowrep@gmail.com>
     Copyright 2019 Sefa Eyeoglu <contact@scrumplex.net>
+    Copyright 2020 Nicolas Fella <nicolas.fella@gmx.de>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
@@ -23,68 +25,184 @@ import QtQuick 2.7
 import QtQuick.Layouts 1.3
 import QtQuick.Controls 2.0
 
-import org.kde.kcm 1.0
+import org.kde.kcm 1.3
 import org.kde.plasma.core 2.0 as PlasmaCore /* for units.gridUnit */
 import org.kde.kirigami 2.5 as Kirigami
 import org.kde.plasma.private.volume 0.1
 
-Kirigami.Page {
-    title: kcm.name
-    property QtObject paSinkModel: SinkModel { }
-    property QtObject paSourceModel: SourceModel { }
+ScrollViewKCM {
+
     property int maxVolumeValue: PulseAudio.NormalVolume // the applet supports changing this value. We will just assume 65536 (100%)
     ConfigModule.quickHelp: i18nd("kcm_pulseaudio", "This module allows configuring the Pulseaudio sound subsystem.")
+
     implicitHeight: Kirigami.Units.gridUnit * 28
     implicitWidth: Kirigami.Units.gridUnit * 28
 
-    // TODO: replace this TabBar-plus-Frame-in-a-ColumnLayout with whatever shakes
-    // out of https://bugs.kde.org/show_bug.cgi?id=394296
-    ColumnLayout {
-        anchors.fill: parent
-        spacing: 0
+    SinkModel {
+        id: paSinkModel
+    }
 
-        TabBar {
-            id: tabView
+    SourceModel {
+        id: paSourceModel
+    }
 
-            // Tab styles generally assume that they're touching the inner layout,
-            // not the frame, so we need to move the tab bar down a pixel and make
-            // sure it's drawn on top of the frame
-            Layout.bottomMargin: -1
-            z: 1
+    PulseObjectFilterModel {
+        id: paSinkFilterModel
 
-            TabButton {
-                text: i18ndc("kcm_pulseaudio", "@title:tab", "Devices")
-            }
-            TabButton {
-                text: i18ndc("kcm_pulseaudio", "@title:tab", "Applications")
-            }
-            TabButton {
-                text: i18ndc("kcm_pulseaudio", "@title:tab", "Advanced")
+        sortRole: "SortByDefault"
+        sortOrder: Qt.DescendingOrder
+        filterOutInactiveDevices: true
+        sourceModel: paSinkModel
+    }
+
+    PulseObjectFilterModel {
+        id: paSourceFilterModel
+
+        sortRole: "SortByDefault"
+        sortOrder: Qt.DescendingOrder
+        filterOutInactiveDevices: true
+        sourceModel: paSourceModel
+    }
+
+    view: Flickable {
+        id: flickable
+
+        contentWidth: column.width
+        contentHeight: column.height
+
+        Item {
+            width: flickable.width
+
+            ColumnLayout {
+                id: column
+
+                anchors.left: parent.left
+                anchors.leftMargin: Kirigami.Units.largeSpacing
+                anchors.right: parent.right
+                anchors.rightMargin: Kirigami.Units.largeSpacing
+                anchors.top: parent.top
+
+                Header {
+                    Layout.fillWidth: true
+                    enabled: sinks.count > 0
+                    text: i18nd("kcm_pulseaudio", "Playback Devices")
+                    disabledText: i18ndc("kcm_pulseaudio", "@label", "No Playback Devices Available")
+                }
+
+                ListView {
+                    id: sinks
+                    Layout.fillWidth: true
+                    Layout.preferredHeight: contentHeight
+                    interactive: false
+                    spacing: Kirigami.Units.gridUnit
+                    model: inactiveDevicesButton.checked || !inactiveDevicesButton.visible ? paSinkModel : paSinkFilterModel
+                    delegate: DeviceListItem {
+                        isPlayback: true
+                    }
+                }
+
+                Header {
+                    Layout.fillWidth: true
+                    enabled: sources.count > 0
+                    text: i18nd("kcm_pulseaudio", "Recording Devices")
+                    disabledText: i18ndc("kcm_pulseaudio", "@label", "No Recording Devices Available")
+                }
+
+                ListView {
+                    id: sources
+                    Layout.fillWidth: true
+                    Layout.preferredHeight: contentHeight
+                    interactive: false
+                    spacing: Kirigami.Units.gridUnit
+                    model: inactiveDevicesButton.checked || !inactiveDevicesButton.visible ? paSourceModel : paSourceFilterModel
+                    delegate: DeviceListItem {
+                        isPlayback: false
+                    }
+                }
+
+                Button {
+                    id: inactiveDevicesButton
+                    Layout.alignment: Qt.AlignHCenter
+                    checkable: true
+                    text: i18nd("kcm_pulseaudio", "Show Inactive Devices")
+                    icon.name: "view-visible"
+
+                    // Only show if there actually are any inactive devices
+                    visible: (paSourceModel.count != paSourceFilterModel.count) || (paSinkModel.count != paSinkFilterModel.count)
+                }
+
+                Header {
+                    Layout.fillWidth: true
+                    enabled: eventStreamView.count || sinkInputView.count
+                    text: i18nd("kcm_pulseaudio", "Playback Streams")
+                    disabledText: i18ndc("kcm_pulseaudio", "@label", "No Applications Playing Audio")
+                }
+
+                ListView {
+                    id: eventStreamView
+                    Layout.fillWidth: true
+                    Layout.preferredHeight: contentHeight
+                    interactive: false
+                    spacing: units.largeSpacing
+                    model: PulseObjectFilterModel {
+                        filters: [ { role: "Name", value: "sink-input-by-media-role:event" } ]
+                        sourceModel: StreamRestoreModel {}
+                    }
+                    delegate: StreamListItem {
+                        deviceModel: paSinkModel
+                        isPlayback: true
+                    }
+                }
+
+                ListView {
+                    id: sinkInputView
+                    Layout.fillWidth: true
+                    Layout.preferredHeight: contentHeight
+                    interactive: false
+                    spacing: units.largeSpacing
+                    model: PulseObjectFilterModel {
+                        filters: [ { role: "VirtualStream", value: false } ]
+                        sourceModel: SinkInputModel {}
+                    }
+                    delegate: StreamListItem {
+                        deviceModel: paSinkModel
+                        isPlayback: true
+                    }
+                }
+
+                Header {
+                    Layout.fillWidth: true
+                    enabled: sourceOutputView.count > 0
+                    text: i18nd("kcm_pulseaudio", "Recording Streams")
+                    disabledText: i18ndc("kcm_pulseaudio", "@label", "No Applications Recording Audio")
+                }
+
+                ListView {
+                    id: sourceOutputView
+                    Layout.fillWidth: true
+                    Layout.preferredHeight: contentHeight
+                    interactive: false
+                    spacing: units.largeSpacing
+                    model: PulseObjectFilterModel {
+                        filters: [ { role: "VirtualStream", value: false } ]
+                        sourceModel: SourceOutputModel {}
+                    }
+
+                    delegate: StreamListItem {
+                        deviceModel: sourceModel
+                        isPlayback: false
+                    }
+                }
             }
         }
-        Frame {
-            Layout.fillWidth: true
-            Layout.fillHeight: true
+    }
 
-            StackLayout {
-                anchors.fill: parent
-                anchors.margins: -Kirigami.Units.smallSpacing
-
-                currentIndex: tabView.currentIndex
-
-                Devices {
-                    Layout.fillWidth: true
-                    Layout.fillHeight: true
-                }
-                Applications {
-                    Layout.fillWidth: true
-                    Layout.fillHeight: true
-                }
-                Advanced {
-                    Layout.fillWidth: true
-                    Layout.fillHeight: true
-                }
-            }
+    footer: RowLayout {
+        Button {
+            text: i18n("Configure...")
+            icon.name: "configure"
+            Layout.alignment: Qt.AlignRight
+            onClicked: kcm.push("Advanced.qml")
         }
     }
 }