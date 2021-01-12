f35fdc1403a19d65010bafa0ed2ce28a2720e9db
Kai Uwe Broulik <kde@privat.broulik.de>
2021-01-12 22:35:16 +0100

    [Volume Monitor] Emit targetChanged signal
    When the property changes

--- src/volumemonitor.cpp
+++ src/volumemonitor.cpp
@@ -83,6 +83,8 @@ void QPulseAudio::VolumeMonitor::setTarget(QPulseAudio::VolumeObject* target)
     if (target) {
         createStream();
     }
+
+    Q_EMIT targetChanged();
 }
 
 void VolumeMonitor::createStream()
