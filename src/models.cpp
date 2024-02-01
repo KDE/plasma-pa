0eb29dc1a9267d20632d2db6a3ebc04fee5b7701
Harald Sitter <sitter@kde.org>
2024-02-01 21:02:15 +0100

    models: import from pulseaudio-qt

--- src/models.cpp
+++ src/models.cpp
@@ -8,34 +8,39 @@
 #include "models.h"
 
 #include "card.h"
+#include "context.h"
 #include "context_p.h"
 #include "debug.h"
+#include "maps.h"
 #include "module.h"
 #include "server.h"
+#include "sink.h"
 #include "sinkinput.h"
+#include "source.h"
 #include "sourceoutput.h"
 #include "streamrestore.h"
 
+#include "models_p.h"
 #include <QMetaEnum>
 
 namespace PulseAudioQt
 {
 AbstractModel::AbstractModel(const MapBaseQObject *map, QObject *parent)
     : QAbstractListModel(parent)
-    , m_map(map)
+    , d(new AbstractModelPrivate(this, map))
 {
-    connect(m_map, &MapBaseQObject::aboutToBeAdded, this, [this](int index) {
+    connect(d->m_map, &MapBaseQObject::aboutToBeAdded, this, [this](int index) {
         beginInsertRows(QModelIndex(), index, index);
     });
-    connect(m_map, &MapBaseQObject::added, this, [this](int index) {
+    connect(d->m_map, &MapBaseQObject::added, this, [this](int index) {
         onDataAdded(index);
         endInsertRows();
         Q_EMIT countChanged();
     });
-    connect(m_map, &MapBaseQObject::aboutToBeRemoved, this, [this](int index) {
+    connect(d->m_map, &MapBaseQObject::aboutToBeRemoved, this, [this](int index) {
         beginRemoveRows(QModelIndex(), index, index);
     });
-    connect(m_map, &MapBaseQObject::removed, this, [this](int index) {
+    connect(d->m_map, &MapBaseQObject::removed, this, [this](int index) {
         Q_UNUSED(index);
         endRemoveRows();
         Q_EMIT countChanged();
@@ -44,16 +49,27 @@ AbstractModel::AbstractModel(const MapBaseQObject *map, QObject *parent)
 
 AbstractModel::~AbstractModel()
 {
+    delete d;
+}
+
+AbstractModelPrivate::AbstractModelPrivate(AbstractModel *q, const MapBaseQObject *map)
+    : q(q)
+    , m_map(map)
+{
+}
+
+AbstractModelPrivate::~AbstractModelPrivate()
+{
 }
 
 QHash<int, QByteArray> AbstractModel::roleNames() const
 {
-    if (!m_roles.empty()) {
-        qCDebug(PLASMAPA) << "returning roles" << m_roles;
-        return m_roles;
+    if (!d->m_roles.empty()) {
+        qCDebug(PULSEAUDIOQT) << "returning roles" << d->m_roles;
+        return d->m_roles;
     }
     Q_UNREACHABLE();
-    return {};
+    return QHash<int, QByteArray>();
 }
 
 int AbstractModel::rowCount(const QModelIndex &parent) const
@@ -61,24 +77,24 @@ int AbstractModel::rowCount(const QModelIndex &parent) const
     if (parent.isValid()) {
         return 0;
     }
-    return m_map->count();
+    return d->m_map->count();
 }
 
 QVariant AbstractModel::data(const QModelIndex &index, int role) const
 {
     if (!hasIndex(index.row(), index.column())) {
-        return {};
+        return QVariant();
     }
-    QObject *data = m_map->objectAt(index.row());
+    QObject *data = d->m_map->objectAt(index.row());
     Q_ASSERT(data);
     if (role == PulseObjectRole) {
         return QVariant::fromValue(data);
     } else if (role == Qt::DisplayRole) {
-        return static_cast<PulseObject *>(data)->properties().value(QStringLiteral("name")).toString();
+        return static_cast<PulseObject *>(data)->name();
     }
-    int property = m_objectProperties.value(role, -1);
+    int property = d->m_objectProperties.value(role, -1);
     if (property == -1) {
-        return {};
+        return QVariant();
     }
     return data->metaObject()->property(property).read(data);
 }
@@ -88,19 +104,19 @@ bool AbstractModel::setData(const QModelIndex &index, const QVariant &value, int
     if (!hasIndex(index.row(), index.column())) {
         return false;
     }
-    int propertyIndex = m_objectProperties.value(role, -1);
+    int propertyIndex = d->m_objectProperties.value(role, -1);
     if (propertyIndex == -1) {
         return false;
     }
-    QObject *data = m_map->objectAt(index.row());
+    QObject *data = d->m_map->objectAt(index.row());
     auto property = data->metaObject()->property(propertyIndex);
     return property.write(data, value);
 }
 
 int AbstractModel::role(const QByteArray &roleName) const
 {
-    qCDebug(PLASMAPA) << roleName << m_roles.key(roleName, -1);
-    return m_roles.key(roleName, -1);
+    qCDebug(PULSEAUDIOQT) << roleName << d->m_roles.key(roleName, -1);
+    return d->m_roles.key(roleName, -1);
 }
 
 Context *AbstractModel::context() const
@@ -110,7 +126,7 @@ Context *AbstractModel::context() const
 
 void AbstractModel::initRoleNames(const QMetaObject &qobjectMetaObject)
 {
-    m_roles[PulseObjectRole] = QByteArrayLiteral("PulseObject");
+    d->m_roles[PulseObjectRole] = QByteArrayLiteral("PulseObject");
 
     QMetaEnum enumerator;
     for (int i = 0; i < metaObject()->enumeratorCount(); ++i) {
@@ -125,13 +141,13 @@ void AbstractModel::initRoleNames(const QMetaObject &qobjectMetaObject)
         const int roleLength = 4;
         QByteArray key(enumerator.key(i));
         // Enum values must end in Role or the enum is crap
-        Q_ASSERT(key.endsWith(QByteArrayLiteral("Role")));
+        Q_ASSERT(key.right(roleLength) == QByteArrayLiteral("Role"));
         key.chop(roleLength);
-        m_roles[enumerator.value(i)] = key;
+        d->m_roles[enumerator.value(i)] = key;
     }
 
     int maxEnumValue = -1;
-    for (auto it = m_roles.constBegin(); it != m_roles.constEnd(); ++it) {
+    for (auto it = d->m_roles.constBegin(); it != d->m_roles.constEnd(); ++it) {
         if (it.key() > maxEnumValue) {
             maxEnumValue = it.key();
         }
@@ -142,17 +158,17 @@ void AbstractModel::initRoleNames(const QMetaObject &qobjectMetaObject)
         QMetaProperty property = mo.property(i);
         QString name(property.name());
         name.replace(0, 1, name.at(0).toUpper());
-        m_roles[++maxEnumValue] = name.toLatin1();
-        m_objectProperties.insert(maxEnumValue, i);
+        d->m_roles[++maxEnumValue] = name.toLatin1();
+        d->m_objectProperties.insert(maxEnumValue, i);
         if (!property.hasNotifySignal()) {
             continue;
         }
-        m_signalIndexToProperties.insert(property.notifySignalIndex(), i);
+        d->m_signalIndexToProperties.insert(property.notifySignalIndex(), i);
     }
-    qCDebug(PLASMAPA) << m_roles;
+    qCDebug(PULSEAUDIOQT) << d->m_roles;
 
     // Connect to property changes also with objects already in model
-    for (int i = 0; i < m_map->count(); ++i) {
+    for (int i = 0; i < d->m_map->count(); ++i) {
         onDataAdded(i);
     }
 }
@@ -162,26 +178,26 @@ void AbstractModel::propertyChanged()
     if (!sender() || senderSignalIndex() == -1) {
         return;
     }
-    int propertyIndex = m_signalIndexToProperties.value(senderSignalIndex(), -1);
+    int propertyIndex = d->m_signalIndexToProperties.value(senderSignalIndex(), -1);
     if (propertyIndex == -1) {
         return;
     }
-    int role = m_objectProperties.key(propertyIndex, -1);
+    int role = d->m_objectProperties.key(propertyIndex, -1);
     if (role == -1) {
         return;
     }
-    int index = m_map->indexOfObject(sender());
-    qCDebug(PLASMAPA) << "PROPERTY CHANGED (" << index << ") :: " << role << roleNames().value(role);
+    int index = d->m_map->indexOfObject(sender());
+    qCDebug(PULSEAUDIOQT) << "PROPERTY CHANGED (" << index << ") :: " << role << roleNames().value(role);
     Q_EMIT dataChanged(createIndex(index, 0), createIndex(index, 0), {role});
 }
 
 void AbstractModel::onDataAdded(int index)
 {
-    QObject *data = m_map->objectAt(index);
+    QObject *data = d->m_map->objectAt(index);
     const QMetaObject *mo = data->metaObject();
     // We have all the data changed notify signals already stored
-    const auto keys = m_signalIndexToProperties.keys();
-    for (const int index : keys) {
+    const auto keys = d->m_signalIndexToProperties.keys();
+    for (const auto &index : keys) {
         QMetaMethod meth = mo->method(index);
         connect(data, meth, this, propertyChangedMetaMethod());
     }
@@ -192,38 +208,15 @@ QMetaMethod AbstractModel::propertyChangedMetaMethod() const
     auto mo = metaObject();
     int methodIndex = mo->indexOfMethod("propertyChanged()");
     if (methodIndex == -1) {
-        return {};
+        return QMetaMethod();
     }
     return mo->method(methodIndex);
 }
 
 SinkModel::SinkModel(QObject *parent)
     : AbstractModel(&context()->d->m_sinks, parent)
-    , m_preferredSink(nullptr)
 {
     initRoleNames(Sink::staticMetaObject);
-
-    for (int i = 0; i < context()->sinks().count(); ++i) {
-        sinkAdded(i);
-    }
-
-    connect(&context()->d->m_sinks, &MapBaseQObject::added, this, &SinkModel::sinkAdded);
-    connect(&context()->d->m_sinks, &MapBaseQObject::removed, this, &SinkModel::sinkRemoved);
-
-    connect(context()->server(), &Server::defaultSinkChanged, this, [this]() {
-        updatePreferredSink();
-        Q_EMIT defaultSinkChanged();
-    });
-}
-
-Sink *SinkModel::defaultSink() const
-{
-    return context()->server()->defaultSink();
-}
-
-Sink *SinkModel::preferredSink() const
-{
-    return m_preferredSink;
 }
 
 QVariant SinkModel::data(const QModelIndex &index, int role) const
@@ -237,89 +230,10 @@ QVariant SinkModel::data(const QModelIndex &index, int role) const
     return AbstractModel::data(index, role);
 }
 
-void SinkModel::sinkAdded(int index)
-{
-    Q_ASSERT(qobject_cast<Sink *>(context()->d->m_sinks.objectAt(index)));
-    Sink *sink = static_cast<Sink *>(context()->d->m_sinks.objectAt(index));
-    connect(sink, &Sink::stateChanged, this, &SinkModel::updatePreferredSink);
-
-    updatePreferredSink();
-}
-
-void SinkModel::sinkRemoved(int index)
-{
-    Q_UNUSED(index);
-
-    updatePreferredSink();
-}
-
-void SinkModel::updatePreferredSink()
-{
-    Sink *sink = findPreferredSink();
-
-    if (sink != m_preferredSink) {
-        qCDebug(PLASMAPA) << "Changing preferred sink to" << sink << (sink ? sink->name() : "");
-        m_preferredSink = sink;
-        Q_EMIT preferredSinkChanged();
-    }
-}
-
-Sink *SinkModel::findPreferredSink() const
-{
-    const auto &sinks = context()->d->m_sinks;
-
-    // Only one sink is the preferred one
-    if (sinks.count() == 1) {
-        return static_cast<Sink *>(sinks.objectAt(0));
-    }
-
-    auto lookForState = [this](Device::State state) {
-        Sink *ret = nullptr;
-        QMapIterator<quint32, Sink *> it(context()->d->m_sinks.data());
-        while (it.hasNext()) {
-            it.next();
-            if (it.value()->state() != state) {
-                continue;
-            }
-            if (!ret) {
-                ret = it.value();
-            } else if (it.value() == defaultSink()) {
-                ret = it.value();
-                break;
-            }
-        }
-        return ret;
-    };
-
-    Sink *preferred = nullptr;
-
-    // Look for playing sinks + prefer default sink
-    preferred = lookForState(Device::RunningState);
-    if (preferred) {
-        return preferred;
-    }
-
-    // Look for idle sinks + prefer default sink
-    preferred = lookForState(Device::IdleState);
-    if (preferred) {
-        return preferred;
-    }
-
-    // Fallback to default sink
-    return defaultSink();
-}
-
 SourceModel::SourceModel(QObject *parent)
     : AbstractModel(&context()->d->m_sources, parent)
 {
     initRoleNames(Source::staticMetaObject);
-
-    connect(context()->server(), &Server::defaultSourceChanged, this, &SourceModel::defaultSourceChanged);
-}
-
-Source *SourceModel::defaultSource() const
-{
-    return context()->server()->defaultSource();
 }
 
 QVariant SourceModel::data(const QModelIndex &index, int role) const
