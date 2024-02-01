321326afeedf947eb8b42be07756e1f815abb743
Harald Sitter <sitter@kde.org>
2024-02-01 21:00:35 +0100

    pulseaudio: rename to models
    in preparation of replacing it with the version from pulseaudio-qt

--- src/models.cpp
+++ src/models.cpp
@@ -5,7 +5,7 @@
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
-#include "pulseaudio.h"
+#include "models.h"
 
 #include "card.h"
 #include "context_p.h"
