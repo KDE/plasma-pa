14ba71a7264fe5bd623d82599d08f067ed43a6ef
Harald Sitter <sitter@kde.org>
2024-02-01 21:11:50 +0100

    maps: import from pulseaudio-qt

--- src/maps.h
+++ src/maps.h
@@ -1,29 +1,28 @@
 /*
     SPDX-FileCopyrightText: 2014-2015 Harald Sitter <sitter@kde.org>
+    SPDX-FileCopyrightText: 2018 David Rosca <nowrep@gmail.com>
 
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
-#ifndef MAPS_H
-#define MAPS_H
+#pragma once
 
-#include "debug.h"
-#include <QMap>
+#include <QHash>
 #include <QObject>
+#include <QSet>
+#include <QVector>
 
 #include <pulse/ext-stream-restore.h>
 #include <pulse/pulseaudio.h>
 
-#include <iterator>
-
-#include "sourceoutput_p.h"
-#include "sinkinput_p.h"
 #include "card_p.h"
-#include "port_p.h"
 #include "client_p.h"
-#include "source_p.h"
-#include "sink_p.h"
 #include "module_p.h"
+#include "sink_p.h"
+#include "sinkinput_p.h"
+#include "source_p.h"
+#include "sourceoutput_p.h"
+#include "streamrestore_p.h"
 
 namespace PulseAudioQt
 {
@@ -36,17 +35,6 @@ class Source;
 class SourceOutput;
 class StreamRestore;
 class Module;
-
-template<typename T>
-concept IsPrivateUpdatable = requires(T t) {
-    {
-        t.d
-    } -> std::convertible_to<void *>;
-    t.d->update(nullptr);
-};
-
-template<typename T>
-concept IsPublicUpdatable = requires(T t) { t.update(nullptr); };
 
 /**
  * @see MapBase
@@ -79,9 +67,11 @@ template<typename Type, typename PAInfo>
 class MapBase : public MapBaseQObject
 {
 public:
-    ~MapBase() override = default;
+    virtual ~MapBase()
+    {
+    }
 
-    const QMap<quint32, Type *> &data() const
+    const QVector<Type *> &data() const
     {
         return m_data;
     }
@@ -93,59 +83,32 @@ public:
 
     int indexOfObject(QObject *object) const override
     {
-        int index = 0;
-        QMapIterator<quint32, Type *> it(m_data);
-        while (it.hasNext()) {
-            it.next();
-            if (it.value() == object) {
-                return index;
-            }
-            index++;
-        }
-        return -1;
+        return m_data.indexOf(static_cast<Type *>(object));
     }
 
     QObject *objectAt(int index) const override
     {
-        return std::next(m_data.constBegin(), index).value();
+        return m_data.at(index);
     }
 
     void reset()
     {
-        while (!m_data.isEmpty()) {
-            removeEntry(m_data.lastKey());
+        while (!m_hash.isEmpty()) {
+            removeEntry(m_data.at(m_data.count() - 1)->index());
         }
         m_pendingRemovals.clear();
     }
 
     void insert(Type *object)
     {
-        Q_ASSERT(!m_data.contains(object->index()));
+        Q_ASSERT(!m_data.contains(object));
 
-        int modelIndex = 0;
-        for (auto it = m_data.constBegin(); it != m_data.constEnd(); ++it) {
-            if (object->index() < it.key()) {
-                break;
-            }
-            modelIndex++;
-        }
+        const int modelIndex = m_data.count();
 
         Q_EMIT aboutToBeAdded(modelIndex);
-        m_data.insert(object->index(), object);
-        Q_ASSERT(modelIndex == m_data.keys().indexOf(object->index()));
+        m_data.append(object);
+        m_hash[object->index()] = object;
         Q_EMIT added(modelIndex, object);
-    }
-
-    template <typename ... Args>
-    void updatePublicOrPrivate(Type *obj, Args && ... args) requires IsPrivateUpdatable<Type>
-    {
-        obj->d->update(args...);
-    }
-
-    template <typename ... Args>
-    void updatePublicOrPrivate(Type *obj, Args && ... args) requires IsPublicUpdatable<Type>
-    {
-        obj->update(args...);
     }
 
     // Context is passed in as parent because context needs to include the maps
@@ -160,44 +123,43 @@ public:
             return;
         }
 
-        auto *obj = m_data.value(info->index, nullptr);
+        auto *obj = m_hash.value(info->index);
         if (!obj) {
             obj = new Type(parent);
-        }
-        updatePublicOrPrivate(obj, info);
-
-        if (!m_data.contains(info->index)) {
+            obj->d->update(info);
             insert(obj);
+        } else {
+            obj->d->update(info);
         }
     }
 
     void removeEntry(quint32 index)
     {
-        if (!m_data.contains(index)) {
+        if (!m_hash.contains(index)) {
             m_pendingRemovals.insert(index);
         } else {
-            const int modelIndex = m_data.keys().indexOf(index);
+            const int modelIndex = m_data.indexOf(m_hash.value(index));
             Q_EMIT aboutToBeRemoved(modelIndex);
-            auto object = m_data.take(index);
+            m_data.removeAt(modelIndex);
+            auto object = m_hash.take(index);
             Q_EMIT removed(modelIndex, object);
-            object->deleteLater();
+            delete object;
         }
     }
 
 protected:
-    QMap<quint32, Type *> m_data;
+    QVector<Type *> m_data;
+    QHash<quint32, Type *> m_hash;
     QSet<quint32> m_pendingRemovals;
 };
 
-using SinkMap = MapBase<Sink, pa_sink_info>;
-using SinkInputMap = MapBase<SinkInput, pa_sink_input_info>;
-using SourceMap = MapBase<Source, pa_source_info>;
-using SourceOutputMap = MapBase<SourceOutput, pa_source_output_info>;
-using ClientMap = MapBase<Client, pa_client_info>;
-using CardMap = MapBase<Card, pa_card_info>;
-using ModuleMap = MapBase<Module, pa_module_info>;
-using StreamRestoreMap = MapBase<StreamRestore, pa_ext_stream_restore_info>;
+typedef MapBase<Card, pa_card_info> CardMap;
+typedef MapBase<Client, pa_client_info> ClientMap;
+typedef MapBase<SinkInput, pa_sink_input_info> SinkInputMap;
+typedef MapBase<Sink, pa_sink_info> SinkMap;
+typedef MapBase<Source, pa_source_info> SourceMap;
+typedef MapBase<SourceOutput, pa_source_output_info> SourceOutputMap;
+typedef MapBase<StreamRestore, pa_ext_stream_restore_info> StreamRestoreMap;
+typedef MapBase<Module, pa_module_info> ModuleMap;
 
 } // PulseAudioQt
-
-#endif // MAPS_H
