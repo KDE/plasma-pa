c92badf5a0189c8bc8f712fe5b6068881ebf6f7a
Marco Martin <notmart@gmail.com>
2024-05-20 11:44:16 +0200

    Use Symbolic icons
    Ask for symbolic icons in the systray, preferring
    monochrome icons if the theme provides both monochrome and colored ones
    
    No visual changes should happen on breeze with this patch

--- src/audioicon.cpp
+++ src/audioicon.cpp
@@ -12,20 +12,20 @@
 using namespace Qt::Literals::StringLiterals;
 
 const QMap<QString, QString> formFactorMap = {
-    {u"internal"_s, u"audio-card"_s},
+    {u"internal"_s, u"audio-card-symbolic"_s},
     {u"speaker"_s, u"audio-speakers-symbolic"_s},
-    {u"phone"_s, u"phone"_s},
-    {u"handset"_s, u"phone"_s},
-    {u"tv"_s, u"video-television"_s},
-    {u"webcam"_s, u"camera-web"_s},
-    {u"microphone"_s, u"audio-input-microphone"_s},
-    {u"headset"_s, u"audio-headset"_s},
-    {u"headphone"_s, u"audio-headphones"_s},
-    {u"hands-free"_s, u"hands-free"_s},
-    {u"car"_s, u"car"_s},
-    {u"hifi"_s, u"hifi"_s},
-    {u"computer"_s, u"computer"_s},
-    {u"portable"_s, u"portable"_s},
+    {u"phone"_s, u"phone-symbolic"_s},
+    {u"handset"_s, u"phone-symbolic"_s},
+    {u"tv"_s, u"video-television-symbolic"_s},
+    {u"webcam"_s, u"camera-web-symbolic"_s},
+    {u"microphone"_s, u"audio-input-microphone-symbolic"_s},
+    {u"headset"_s, u"audio-headset-symbolic"_s},
+    {u"headphone"_s, u"audio-headphones-symbolic"_s},
+    {u"hands-free"_s, u"hands-free-symbolic"_s},
+    {u"car"_s, u"car-symbolic"_s},
+    {u"hifi"_s, u"hifi-symbolic"_s},
+    {u"computer"_s, u"computer-symbolic"_s},
+    {u"portable"_s, u"portable-symbolic"_s},
 };
 
 QString AudioIcon::forFormFactor(QString formFactor)
@@ -39,16 +39,16 @@ QString AudioIcon::forVolume(int percent, bool muted, QString prefix)
         prefix = u"audio-volume"_s;
     }
     if (percent <= 0 || muted) {
-        return prefix + "-muted";
+        return prefix + "-muted-symbolic";
     } else if (percent <= 25) {
-        return prefix + "-low";
+        return prefix + "-low-symbolic";
     } else if (percent <= 75) {
-        return prefix + "-medium";
+        return prefix + "-medium-symbolic";
     } else if (percent <= HIGH_UPPER_BOUND) {
-        return prefix + "-high";
+        return prefix + "-high-symbolic";
     } else if (percent <= VERY_HIGH_UPPER_BOUND) {
-        return prefix + "-high-warning";
+        return prefix + "-high-warning-symbolic";
     } else {
-        return prefix + "-high-danger";
+        return prefix + "-high-danger-symbolic";
     }
 }
