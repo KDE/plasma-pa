Sonic-DE/sonic-audio-applet-pulse

Add hamburger menu in plasma-pa applet
This makes the applet consistent with its system tray
representation.

BUG:497869

applet/contents/ui/main.qml
@@ -310,19 +310,38 @@ PlasmoidItem {
                 }
 
                 PC3.ToolButton {
-                    id: globalMuteCheckbox
+                    id: actionsButton
 
                     visible: !(plasmoid.containmentDisplayHints & PlasmaCore.Types.ContainmentDrawsPlasmoidHeading)
 
-                    icon.name: "audio-volume-muted"
-                    onClicked: {
-                        GlobalService.globalMute()
+                    icon.name: "application-menu"
+                    checkable: true
+                    checked: configMenu.status !== PlasmaExtras.Menu.Closed
+                    display: PlasmaComponents.AbstractButton.IconOnly
+                    onToggled: {
+                        if (checked) {
+                            configMenu.openRelative();
+                        } else {
+                            configMenu.close();
+                        }
                     }
-                    checked: globalMute
 
-                    Accessible.name: i18n("Force mute all playback devices")
+                    Accessible.name: i18n("More actions")
                     PC3.ToolTip {
-                        text: i18n("Force mute all playback devices")
+                        text: i18n("More actions")
+                    }
+                }
+
+                PlasmaExtras.Menu {
+                    id: configMenu
+                    visualParent: actionsButton
+                    placement: PlasmaExtras.Menu.BottomPosedLeftAlignedPopup
+
+                    PlasmaExtras.MenuItem {
+                        action: Plasmoid.contextualActions[0]
+                    }
+                    PlasmaExtras.MenuItem {
+                        action: Plasmoid.contextualActions[1]
                     }
                 }
 

KDE/plasma-pa

applet/contents/ui/main.qml
@@ -310,19 +310,44 @@ PlasmoidItem {
                 }
 
                 PC3.ToolButton {
-                    id: globalMuteCheckbox
+                    id: actionsButton
 
                     visible: !(plasmoid.containmentDisplayHints & PlasmaCore.Types.ContainmentDrawsPlasmoidHeading)
 
-                    icon.name: "audio-volume-muted"
-                    onClicked: {
-                        GlobalService.globalMute()
+                    icon.name: "application-menu"
+                    display: PC3.AbstractButton.IconOnly
+                    checkable: true
+                    checked: configMenu.status !== PlasmaExtras.Menu.Closed
+                    onToggled: {
+                        if (checked) {
+                            configMenu.openRelative();
+                        } else {
+                            configMenu.close();
+                        }
                     }
-                    checked: globalMute
 
-                    Accessible.name: i18n("Force mute all playback devices")
+                    text: i18nc("@action:button", "More actions")
                     PC3.ToolTip {
-                        text: i18n("Force mute all playback devices")
+                        text: parent.text
+                    }
+                }
+
+                PlasmaExtras.Menu {
+                    id: configMenu
+                    visualParent: actionsButton
+                    placement: PlasmaExtras.Menu.BottomPosedLeftAlignedPopup
+                }
+
+                Instantiator {
+                    model: Plasmoid.contextualActions
+                    delegate: PlasmaExtras.MenuItem {
+                        id: menuItem
+                        action: modelData
+                    }
+                    onObjectAdded: (index, object) => {
+                        if (object.action.priority === PlasmaCore.Action.NormalPriority) {
+                            configMenu.addMenuItem(object);
+                        }
                     }
                 }
 