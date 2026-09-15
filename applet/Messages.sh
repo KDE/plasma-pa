10f9d90348eb59e1914d14da629dc1a21ad9cc2e
Harald Sitter <sitter@kde.org>
2026-09-15 14:30:44 +0200

    Messages.sh: extract qml as javascript
    ensures we can use all string types properly
    
    files copied out of kscreenlocker

--- applet/Messages.sh
+++ applet/Messages.sh
@@ -1,2 +1,12 @@
-#! /usr/bin/env bash
-$XGETTEXT `find . -name \*.js -o -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.volume.pot
+#!/bin/sh
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2020-2025 Harald Sitter <sitter@kde.org>
+
+# Ensure passing shellcheck:
+# Our l10n scripting isn't working with spaces anywhere and we actively rely on word splitting in our Messages.sh.
+# shellcheck disable=SC2046
+podir=${podir:?} # ensure it is defined
+
+# Extract JavaScripty files as what they are, otherwise for example template literals won't work correctly (by default we extract as C++).
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+$XGETTEXT --language=JavaScript $(find . -name \*.qml -o -name \*.js) -o "$podir"/plasma_applet_org.kde.plasma.volume.pot
